7b9dec2bb73a5c71ec03e14280a3fb1c1bc5bd9f
Anne van Gorkom <anne@vangorkom.com>
2018-01-17 12:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine[i]} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 //Build a function that a new customer will use when entering the deli.
