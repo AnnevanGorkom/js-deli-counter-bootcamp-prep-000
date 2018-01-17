f2c35f05bd69ed0ba711234e2df43c39111155d9
Anne van Gorkom <anne@vangorkom.com>
2018-01-17 13:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  var lineWithNumber = [];
+
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
-  }
+  }  
   for (var i = 0; i < katzDeliLine.length; i++) {
-    var lineWithNumber = [];
     lineWithNumber.push(`${i + 1}. ${katzDeliLine[i]}`);
   }
   return `The line is currently: ${lineWithNumber.join(", ")}`;
