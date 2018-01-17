a5e7659db087419e13d65200dbe82e198ddce25a
Anne van Gorkom <anne@vangorkom.com>
2018-01-17 13:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    if (katzDeliLine.length === 0) {
-      return "The line is currently empty."
-    } else {
-      return katzDeliLine;
-    }
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
   }
 }
