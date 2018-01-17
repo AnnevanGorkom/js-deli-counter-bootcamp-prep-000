5a8f148e3d5996d7b724c2fcb14ef0b5348bce88
Anne van Gorkom <anne@vangorkom.com>
2018-01-17 13:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(katzDeliLine, name) {
-  for (var i = 0; i <= katzDeliLine.length; i++) {
-    if (katzDeliLine.length === 0) {
-      return "There is nobody waiting to be served!";
-    } else {
-      return `Currently serving ${katzDeliLine[0]}.`;
-    }
-    katzDeliLine.shift();
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
