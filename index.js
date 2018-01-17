901fcd00ebf4ed7160d3fd447af2c149a5313286
Anne van Gorkom <anne@vangorkom.com>
2018-01-17 13:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function nowServing(katzDeliLine, name) {
     return "There is nobody waiting to be served!";
   } else {
     return `Currently serving ${name}.`;
+    katzDeliLine.shift();
   }
 }
 
