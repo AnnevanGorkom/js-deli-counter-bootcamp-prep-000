fe3428a6669cf4a70bfd1403ab0027dde6b9cb75
Anne van Gorkom <anne@vangorkom.com>
2018-01-17 12:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    katzDeliLine.push(name);
-  }
+  katzDeliLine.push(name);
   return `Welcome, ${name}. You are number 1 in line.`;
 }
 
